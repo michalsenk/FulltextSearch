afbb25f0ddb6ca38242c0b8d9d6f0e4fee0a970b
senkm <michal.senk@russmedia.com>
2022-09-29 14:54:58 +0200

    Initial Commit

---
+++ FulltextSearch/FulltextSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FulltextSearchApp.swift
+//  FulltextSearch
+//
+//  Created by Michal Šenk on 29.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FulltextSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FulltextSearchUITests/FulltextSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FulltextSearchUITestsLaunchTests.swift
+//  FulltextSearchUITests
+//
+//  Created by Michal Šenk on 29.09.2022.
+//
+
+import XCTest
+
+final class FulltextSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
