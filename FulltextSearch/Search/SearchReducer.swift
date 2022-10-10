b82e0bd4ae5f59f409ae0806fe3cd09e54f3bcc7
senkm <michal.senk@russmedia.com>
2022-10-10 11:37:37 +0200

    Live service, decoded mole

---
+++ FulltextSearch/Search/SearchReducer.swift
@@ -0,0 +1,8 @@
+//
+//  SearchReducer.swift
+//  FulltextSearch
+//
+//  Created by Michal Šenk on 08.10.2022.
+//
+
+import Foundation
---
+++ FulltextSearch/Search/SearchState.swift
@@ -0,0 +1,8 @@
+//
+//  SearchState.swift
+//  FulltextSearch
+//
+//  Created by Michal Šenk on 08.10.2022.
+//
+
+import Foundation
